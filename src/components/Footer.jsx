e05f1ea31a7c7563efeb06e9ac64d222cbda20c1
MarIsa-17 <nunezlopezmarisa90@gmail.com>
2025-11-27 16:14:07 -0500

    estilos y responsivo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,13 +2,13 @@ import Copyright from "./Copyright"
 
 function Footer() {
   return (
-    <div>
+    <footer>
       <h3>Isabel Nuñez López</h3>
       <p>📱 Teléfono: +51 987682671</p>
       <p>✉️ Email: marisa@email.com</p>
       
       <Copyright name="Isabel Nuñez"/>
-    </div>
+    </footer>
   )
 }
 export default Footer;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,11 @@
 
 function Header() {
-    const contactImportantes = {
-      background: "#fff1",
-      width: "100vw"
-    }
-    const parrafo ={
-
-        fontSize:"20px",
-    }    
+   
     
   return (
-    <header style={contactImportantes}>
+    <header>
       <h1>📞 Contact Manager</h1>
-      <p style={parrafo}>Mis contactos importantes</p>
+      <p>Mis contactos importantes</p>
     </header>
   )
 }
